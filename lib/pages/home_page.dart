fb1437c4d60bde347b9169fc36051704ed995471
Gustavo Roldão <gustavodsroldao@gmail.com>
2024-07-11 21:04:15 -0300

    dojo

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
-
-import 'package:dojo/widget/card_widget.dart';
+import 'package:shared_preferences/widget/card_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
